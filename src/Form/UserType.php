a37e07de250391429813fe957c3adc57ed4a52f0
Aminebncd <amine.bounachada@hotmail.com>
2024-03-19 15:47:02 +0100

    ajust. mineurs

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -17,26 +18,52 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control column'
+                ]
+            ])
             // ->add('roles')
             // ->add('password')
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control column'
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control column'
+                ]
+            ])
             ->add('dateNaissance', DateType::class, [
                 'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control column'
+                ]
             ])
             ->add('sexe', ChoiceType::class, [
                 'choices' => [
                      'homme' => 'homme',
                      'femme' => 'femme',
                      'autre'=> 'autre'
+                ],
+                'attr' => [
+                    'class' => 'form-control column'
                 ]
             ])
-            ->add('ville')
-            ->add('telephone')
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control column'
+                ]
+            ])
+            ->add('telephone', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control column'
+                ]
+            ])
             ->add('valider', SubmitType::class, [
                 'attr' => [
-                    // 'class' => 'btn btn-primary mt-3'
+                    'class' => 'btn btn-primary'
                     ]
                 ])
             
