6c985ad2d05a65b8e8c1e2c018b1a2cebb87451e
aminebncd <amine.bounachada@hotmail.com>
2024-03-20 15:05:58 +0100

    travail fixtures

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.querySelector('.burger-menu');
+    const burgerMenu = document.querySelector('.burger-toggler');
     const navbar = document.querySelector('.navbar');
 
     burgerMenu.addEventListener('click', function () {
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,26 +2,51 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Session;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Session;
+use App\Entity\Formation;
+use App\Entity\Formateur;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i < 20; $i++) {
-            
+        for ($i = 0; $i < 30; $i++) {
+
+            $formations = $manager->getRepository(Formation::class)->findAll();
+            $formateurs = $manager->getRepository(Formateur::class)->findAll();
+            $createur = $this->getUser();
+    
             $session = new Session();
-            $session->setIntituleSession('session '.$i);
-            $session->setCreateur(mt_rand(1, 2));
-            $session->setFormateur(mt_rand(1, 2));
-            $session->setFormation(mt_rand(1, 3));
+            $session->setIntituleSession('Formation ' . ($i + 1));
+
+            // Génération de dates de début et de fin aléatoires entre le 1er janvier 2024 et le 31 décembre 2024
+            $dateDebut = new \DateTime();
+            $dateDebut->setTimestamp(mt_rand(strtotime('2024-01-01'), strtotime('2024-12-31')));
+            $session->setDateDebut($dateDebut);
+            $dateFin = clone $dateDebut;
+            $dateFin->modify('+' . mt_rand(1, 5) . ' days'); // Durée aléatoire entre 1 et 5 jours
+            $session->setDateFin($dateFin);
+
             $session->setNombrePlaces(mt_rand(10, 50));
-            $session->setDateDebut('Y-m-d H:i:s', mt_rand(time(), strtotime('+2 years')));
-            $session->setDateFin('Y-m-d H:i:s', mt_rand(time(), strtotime('+2 years')));
+
+            // Choix aléatoire d'une formation parmi celles existantes
+            $formation = $formations[array_rand($formations)];
+            $session->setFormation($formation);
+
+            // Choix aléatoire d'un formateur parmi ceux existants
+            $formateur = $formateurs[array_rand($formateurs)];
+            $session->setFormateur($formateur);
+
+            // Liaison avec le créateur de session
+            $session->setCreateur($createur);
+
+            // Enregistrer la session dans la base de données
             $manager->persist($session);
         }
+
         $manager->flush();
     }
 }
